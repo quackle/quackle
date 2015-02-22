507d5e9519779c4493b82cf95077810620f7d5e9
alkamid <adamkli@gmail.com>
2015-02-22 12:05:48 +0000

    find negative leave equity

---
+++ test/findNegativeLeaves.py
@@ -0,0 +1,16 @@
+import io
+
+def findNegativeLeaves():
+    '''sometimes Quackle incorrectly assigns negative equity to a 7-letter rack.
+    This script finds them.'''
+
+    leaves = io.open("input/leaves.txt", encoding='utf-8').read().splitlines()
+    leavesNeg = io.open("output/leaves-negative.txt", "w", encoding='utf-8')
+
+    for line in leaves:
+        if '-' in line[10:]:
+            leavesNeg.write(u'%s\n' % line[:7])
+
+    leavesNeg.close()
+
+findNegativeLeaves()
