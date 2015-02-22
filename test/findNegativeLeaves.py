75ee5889f3b220e35a4532e8e58d0e131b1bbf37
alkamid <adamkli@gmail.com>
2015-02-22 12:21:52 +0000

    output also a filtered list

--- test/findNegativeLeaves.py
+++ test/findNegativeLeaves.py
@@ -6,11 +6,15 @@ def findNegativeLeaves():
 
     leaves = io.open("input/leaves.txt", encoding='utf-8').read().splitlines()
     leavesNeg = io.open("output/leaves-negative.txt", "w", encoding='utf-8')
+    leavesFiltered = io.open("output/leaves.txt", 'w', encoding='utf-8')
 
     for line in leaves:
         if '-' in line[10:]:
             leavesNeg.write(u'%s\n' % line[:7])
+        else:
+            leavesFiltered.write(u'%s\n' % line)
 
     leavesNeg.close()
+    leavesFiltered.close()
 
 findNegativeLeaves()
