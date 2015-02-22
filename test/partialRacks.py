528df411ba9fd0431136913a08e23eca9dfd4f06
alkamid <adamkli@gmail.com>
2015-02-22 08:46:04 +0000

    cosmetics
    added short docs, changed file names

--- test/partialRacks.py
+++ test/partialRacks.py
@@ -5,8 +5,8 @@ import itertools
 import time
 
 def loadAlphabet(filename):
-    fil = io.open(filename, encoding="utf-8")
-    lines = fil.readlines()
+    lines = io.open(filename, encoding="utf-8").read().splitlines()
+    
     letters = {}
     for line in lines:
         line = line.split()
@@ -14,20 +14,30 @@ def loadAlphabet(filename):
             letters[u'?'] = int(line[2])
         else:
             letters[u'%s' % line[0]] = int(line[3])
-    fil.close()
+    
     return letters
 
 def adjustBag(rack, bag):
+    '''take out from the bag the letters that constitue a rack'''
+
     newBag = bag.copy()
     for letter in rack:
         newBag[letter] -= 1
     return newBag
 
 def partialValue(rackLen):
+    '''Calculates the values of partial rack leaves. We need:
+    - an alphabet file with letter distributions
+    - a list of racks that we want to evaluate
+    - a list of evaluated 7-letter racks
+    The script evaluates a weighted average of all 7-letter racks
+    that can be created using a given partial rack. A better program would
+    evaluate them in-play to account for the letters in the bag, but Quackle
+    doesn't allow for this.'''
     
     bag = loadAlphabet("../data/alphabets/polish.quackle_alphabet")
-    racks = io.open("racks%d.txt" % rackLen, encoding='utf-8').read().splitlines()
-    leaveList = io.open("leavesSorted.txt", encoding='utf-8').read().splitlines()
+    racks = io.open("input/racks%d-sorted.txt" % rackLen, encoding='utf-8').read().splitlines()
+    leaveList = io.open("input/leaves-sorted.txt", encoding='utf-8').read().splitlines()
 
     leaves = {}
 
@@ -36,7 +46,7 @@ def partialValue(rackLen):
         (lv, val) = line.split()
         leaves[lv] = float(val)
 
-    outFile = io.open("rack%doutput.txt" % rackLen, 'w', encoding='utf-8')
+    outFile = io.open("output/rack%doutput.txt" % rackLen, 'w', encoding='utf-8')
 
     for r in racks:
         smallerBag = adjustBag(r, bag)
@@ -64,4 +74,6 @@ def partialValue(rackLen):
         avgVal = sumVals / i
         outFile.write(u'%s %f\n' % (r, avgVal))
 
-partialValue(3)
+
+for i in range(2,7):
+    partialValue(i)
