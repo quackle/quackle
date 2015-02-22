2b2f6c36b31b7b731e39d7c6de639c44ba4aff25
alkamid <adamkli@gmail.com>
2015-02-22 17:12:13 +0000

    average leave values

---
+++ test/calculateAverage.py
@@ -0,0 +1,25 @@
+#-*- coding: utf-8 -*-
+
+import numpy as np
+import io
+
+def calculateAverage():
+    '''real values of leaves should be averaged over all possible leaves'''
+    
+    # I don't really know how this conv works, but I'll keep it for now as I've already spent too much time on this script
+    conv = {0:(lambda s: s.decode('utf-8'))}
+
+    values = np.loadtxt("input/leaves.txt", usecols=(1,))
+    leaves = np.loadtxt("input/leaves.txt", dtype='unicode', converters=conv, usecols=(0,))
+
+    newValues =  values - np.mean(values)
+
+    outputFile = io.open("output/leaves.txt", 'w')
+
+    for i, elem in enumerate(leaves):
+        elem
+        outputFile.write(u'%s %f\n' % (leaves[i], newValues[i]))
+
+    outputFile.close()
+
+calculateAverage()
