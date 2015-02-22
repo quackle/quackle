36c76f73372b0804a4204ab651acbf6b956284ad
alkamid <adamkli@gmail.com>
2015-02-22 08:59:20 +0000

    a bash script to run the sims

---
+++ test/runner.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#This is for running multiple game simulations in manageable chunks.
+#Just specify the number of reps per simulation (50k takes 1h on a i5
+#processor) and the random seed for reproducibility
+#./runner 50000 1
+
+reps=$1
+i=$2
+
+while true
+do
+    ./test --alphabet=polish --lexicon=polish --mode=playability --repetitions=1 --seed=$i >> output/playability-reps$reps-seed$i
+    ((i++))
+done
