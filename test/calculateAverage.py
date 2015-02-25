317854bdea9425c2bbad62f3dba1ba6439a97809
alkamid <adamkli@gmail.com>
2015-02-25 08:06:31 +0000

    optimised numpy, takes 1min on i7
    there are two functions for calculating averages now, but one is
    slower than the other and saves (np.savetxt) non-decoded strings.
    So use calculateAverage() only, the other one is left for testing.

--- test/calculateAverage.py
+++ test/calculateAverage.py
@@ -1,25 +1,30 @@
 #-*- coding: utf-8 -*-
 
 import numpy as np
-import io
 
 def calculateAverage():
     '''real values of leaves should be averaged over all possible leaves'''
     
-    # I don't really know how this conv works, but I'll keep it for now as I've already spent too much time on this script. Got it from here: http://stackoverflow.com/questions/28454584/unicode-string-in-matplotlib-annotate
-    conv = {0:(lambda s: s.decode('utf-8'))}
-
     values = np.loadtxt("input/leaves.txt", usecols=(1,))
-    leaves = np.loadtxt("input/leaves.txt", dtype='unicode', converters=conv, usecols=(0,))
+    leaves = np.loadtxt("input/leaves.txt", dtype='str', usecols=(0,))
 
-    newValues =  values - np.mean(values)
-
-    outputFile = io.open("output/leaves.txt", 'w')
+    values -= np.mean(values)
+    outputFile = open("output/leaves.txt", 'w')
 
     for i, elem in enumerate(leaves):
-        elem
-        outputFile.write(u'%s %f\n' % (leaves[i], newValues[i]))
+        outputFile.write('%s %f\n' % (leaves[i], values[i]))
 
     outputFile.close()
+
+def calculateAverage1():
+	#this is SLOWER than calculateAverage() and saves a non-decoded file
+    '''real values of leaves should be averaged over all possible leaves'''
+    
+    values = np.loadtxt("input/leaves.txt", dtype=[('key', 'S8'), ('val', 'f8')])
+
+    values['val'] -= np.mean(values['val'])
+
+    np.savetxt("output/leaves.txt", values, fmt='%s %f')
+
 
 calculateAverage()
