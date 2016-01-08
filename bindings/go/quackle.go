4b4fca57042535e176f061823382c26818d3d995
H. İbrahim Güngör <igungor@gmail.com>
2016-01-08 12:07:35 +0200

    bindings/go: Use static libraries for Go bindings
    Search for the static libraries in their relative paths instead of
    symlinking into the build directory.

--- bindings/go/quackle.go
+++ bindings/go/quackle.go
@@ -2,5 +2,5 @@ package quackle
 
 // #cgo CXXFLAGS: -I../..
 // #cgo pkg-config: QtCore
-// #cgo LDFLAGS: -L${SRCDIR} -lquackle -lquackleio -lQtCore
+// #cgo LDFLAGS: -L${SRCDIR}/../../lib/release -L${SRCDIR}/../../quackleio/lib/release -lquackle -lquackleio -lQtCore
 import "C"
