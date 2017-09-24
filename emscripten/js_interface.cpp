02b25bbdabfe640197d642686f038579f45ae1c6
Cesar Del Solar <delsolar@gmail.com>
2017-09-23 18:25:16 -0700

    simulation

--- emscripten/js_interface.cpp
+++ emscripten/js_interface.cpp
@@ -63,7 +63,6 @@ void loadGameAndPlayers(QString gcgfilename) {
     // player move generator.
     // Later add simming.
     bool playerFound = false;
-    cout << "OK" << endl;
     const Quackle::Player &player = dataManager.computerPlayers().playerForName(
         "Speedy Player", playerFound);
     if (!playerFound) {
@@ -72,17 +71,45 @@ void loadGameAndPlayers(QString gcgfilename) {
     }
     Quackle::ComputerPlayer *computerPlayer = player.computerPlayer();
     QuackleIO::GCGIO io;
-    cout << "HEHE" << endl;
     Quackle::Game *game = io.read(gcgfilename, QuackleIO::Logania::MaintainBoardPreparation);
-    cout << "EEP" << endl;
     // We have a game now.
     Quackle::GamePosition position = game->currentPosition();
-    computerPlayer->setPosition(position);
-    Quackle::MoveList moves = computerPlayer->moves(15);
-    cout << "HERE" << endl;
-    for (Quackle::MoveList::const_iterator it = moves.begin(); it != moves.end(); ++it) {
-        cout << *it << endl;
+    // computerPlayer->setPosition(position);
+    // Quackle::MoveList moves = computerPlayer->moves(15);
+    // for (Quackle::MoveList::const_iterator it = moves.begin(); it != moves.end(); ++it) {
+    //     cout << *it << endl;
+    // }
+    // //
+    const int kibitzLength = 3;
+    game->currentPosition().kibitz(kibitzLength);
+    cout << game->currentPosition().moves() << endl;
+    Quackle::Simulator simulator;
+    simulator.setLogfile("quackletest.simulation", false);
+    simulator.setPosition(game->currentPosition());
+    // simulator.simulate(2, 10);
+    // cout << simulator.simmedMoves() << endl;
+    // cout << "after " << simulator.iterations() << " iterations pruning to those within 5 pts";
+
+    // simulator.pruneTo(5, kibitzLength);
+    int iterationStep = 10;
+    int iterationsToRun = 50;
+
+    const int plies = 2;
+    simulator.setIgnoreOppos(false);
+    cout << "--------------" << endl;
+    for (int iterations = 0; iterations < iterationsToRun; iterations += iterationStep) {
+        simulator.simulate(plies, iterationStep);
+        cout << "sim results after " << iterations + iterationStep << " iterations: " << endl;
+        const Quackle::SimmedMoveList &moves = simulator.simmedMoves();
+
+        for (Quackle::SimmedMoveList::const_iterator it = moves.begin(); it != moves.end(); ++it)
+        {
+            cout << *it << endl;
+        }
+
+        cout << endl;
     }
+
 }
 
 int main(int argc, char **argv) {
