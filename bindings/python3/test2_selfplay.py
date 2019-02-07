d387d2e719f736d28db797d47ffc4fca29a202fa
Martin DeMello <mdemello@google.com>
2019-02-06 20:11:58 -0800

    Add separate python2 and python3 targets, and a flag for Qt version.

--- bindings/python3/test2_selfplay.py
+++ bindings/python3/test2_selfplay.py
@@ -51,11 +51,11 @@ p2 = getComputerPlayer(dm)
 # Create computer players
 player1 = quackle.Player('Compy1', quackle.Player.ComputerPlayerType, 0)
 player1.setComputerPlayer(p1)
-print player1.name()
+print(player1.name())
 
 player2 = quackle.Player('Compy2', quackle.Player.ComputerPlayerType, 1)
 player2.setComputerPlayer(p2)
-print player2.name()
+print(player2.name())
 
 dm.seedRandomNumbers(42)
 
@@ -71,14 +71,15 @@ game.addPosition()
 
 for i in range(50):
     if game.currentPosition().gameOver():
-        print "GAME OVER"
+        print("GAME OVER")
         break
 
     player = game.currentPosition().currentPlayer()
+    print("Player: " + player.name())
+    print("Rack : " + player.rack().toString())
+
     move = game.haveComputerPlay()
-    #print "Player: " + player.name()
-    print "Rack : " + player.rack().toString()
-    print 'Move: ' + move.toString()
-    print 'Board: \n' + game.currentPosition().board().toString()
+    print('Move: ' + move.toString())
+    print('Board: \n' + game.currentPosition().board().toString())
 
     time.sleep(1)
