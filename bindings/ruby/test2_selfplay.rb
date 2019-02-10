d2b078ad15fcadb5de8ef25b8f19bde613f59a41
Martin DeMello <mdemello@google.com>
2019-02-09 18:11:01 -0800

    add support for board configuration

--- bindings/ruby/test2_selfplay.rb
+++ bindings/ruby/test2_selfplay.rb
@@ -1,7 +1,18 @@
 require_relative 'quackle_runner'
 
+# Allow a board file to be passed in on the command line
+if ARGV[0]
+  b = IO.read(ARGV[0])
+  board = Quackle::BoardUtils::read_board_params(
+    b, doublespaced: true, piped: true)
+else
+  # use the default empty board
+  board = nil
+end
+
 runner = Quackle::AIRunner.new(
-  lexicon: 'twl06', alphabet: 'english', datadir: '../../data', random_seed: 42)
+  lexicon: 'csw15', alphabet: 'english', board: board,
+  datadir: '../../data', random_seed: 42)
 
 runner.init_game([["Compy1", :speedy],
                   ["Compy2", :speedy]])
