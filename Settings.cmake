741ba7f9223071da84146069851b48d2344ac03e
John Fultz <jfultz@wolfram.com>
2023-07-16 09:43:11 -0500

    Set the C++ standard to C++17.
    C++17 is a requirement for Qt6.  It’s been out long enough that
    I think we’re safe just requiring it globally.

--- Settings.cmake
+++ Settings.cmake
@@ -1,6 +1,6 @@
 include_guard()
 
 message("-- Including settings")
-set(CMAKE_CXX_STANDARD 14)
+set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_OSX_DEPLOYMENT_TARGET 10.10)
