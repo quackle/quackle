4491f8b0cd53e17eb33aa72a17eaf3145e1236a7
John Fultz <jfultz@wolfram.com>
2023-07-19 10:21:18 -0500

    Remove macOS deployment target for now.
    This will hopefully fix the GitHub actions build.
    Definitely fixes my local build.  This won’t stand
    for a deployment to users, but it works for developing.

--- Settings.cmake
+++ Settings.cmake
@@ -3,4 +3,12 @@ include_guard()
 message("-- Including settings")
 set(CMAKE_CXX_STANDARD 17)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
-set(CMAKE_OSX_DEPLOYMENT_TARGET 10.10)
+
+# Commented out so that automated builds can work
+# but we'll need to set this and set up matching Qt libs
+# for building a final app bundle
+#
+# Note that Qt6 uses std::filesystem which requires minimum of 10.15
+# Qt5 can be set lower (was working fine with 10.10)
+#
+# set(CMAKE_OSX_DEPLOYMENT_TARGET 10.15)
